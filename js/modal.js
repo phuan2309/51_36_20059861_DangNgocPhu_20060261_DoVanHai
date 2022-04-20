08ec2803f66fc56f05a89f10ad13218abb2c4322
phuan2309 <phudang2206@gmail.com>
2022-04-20 23:24:51 +0700

    51_36_20059861_DangNgocPhu_20060261_DoVanHai

--- js/modal.js
+++ js/modal.js
@@ -1,45 +1,14 @@
-
- function ktname(){
-    var name=document.getElementById("name").value;
-    var reg=/^[A-Za-z0-9_\.]{6,32}$/
-    if(reg.test(name)){
-        document.getElementById("err").innerHTML="(*)";
-        return true;
-    }else{
-        document.getElementById("err").innerHTML="(*)Sai";
-        return false;
+function KTMail(){
+    var mail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)+(\.\w{2,3})+$/;
+    if($("#email").val() == ""){
+    $("#err1").html("* Bắt buộc nhập");
+    return false;
     }
-}
-function ktemail(){
-    var email=document.getElementById("email").value;
-    var reg=/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if(reg.test(email)){
-        document.getElementById("err1").innerHTML="(*)";
-        return true;
-    }else{
-        document.getElementById("err1").innerHTML="(*)Sai";
-        return false;
+    if(!mail.test($("#txtMail").val())){
+    $("#err1").html("* Nhập đúng định dạng mail");
+    return false;
     }
-}
-function ktmk(){
-    var mk=document.getElementById("pass").value;
-    var reg= /^[\w-@#$%^&*!]{8,}$/;
-    if(reg.test(mk)){
-        document.getElementById("err2").innerHTML="(*)";
-        return true;
-    }else{
-        document.getElementById("err2").innerHTML="(*)Sai";
-        return false;
+    $("#err1").html("*");
+    return true;
     }
-}
-function ktrmk(){
-    var mk=document.getElementById("pass").value;
-    var rmk=document.getElementById("rpass").value;
-    if(mk==rmk){
-        document.getElementById("err3").innerHTML="(*)";
-        return true;
-    }else{
-        document.getElementById("err3").innerHTML="(*)Sai";
-        return false;
-    }
-}
+    $("#email").blur(KTMail);
